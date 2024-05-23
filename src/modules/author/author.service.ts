58bb3d9c1e61a62c38a48361fcff4e8eeb055a37
NazariiB <nazikbodna3@gmail.com>
2024-05-23 23:10:23 +0300

    add builder pattern

--- src/modules/author/author.service.ts
+++ src/modules/author/author.service.ts
@@ -20,8 +20,19 @@ export class AuthorService {
     return this.authorRepository.find();
   }
 
-  findOne(id: number) {
-    return this.authorRepository.findOneBy({ id });
+  async findOne(id: number) {
+    const queryBuilder = this.authorRepository.createQueryBuilder('author');
+
+    const authorData = await queryBuilder
+      .select(['id', 'full_name as fullName'])
+      .where('id = :id', { id })
+      .getRawOne();
+
+    const author = this.authorRepository.create();
+    author.id = authorData.id;
+    author.fullName = authorData.fullName;
+
+    return author;
   }
 
   update(id: number, updateAuthorDto: AuthorDto) {
